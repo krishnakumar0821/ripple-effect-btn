98b666fb6953a93ae69870d5be56e47f2ce4133a
Krishna Kumar <110533084+krishnakumar0821@users.noreply.github.com>
2023-06-01 21:54:07 +0530

    file added

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+    const x = event.pageX - btnEl.offsetLeft;
+    const y = event.pageY - btnEl.offsetTop;
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+});
+
+
+
+
+// const btnEl = document.querySelector(".btn");
+
+// btnEl.addEventListener("mouseover", (event) => {
+//     console.log(event.pageX - btnEl.offsetLeft);
+//     console.log(event.pageY - btnEl.offsetTop);
+
+//     btnEl.style.setProperty("--xPos", x + "px");
+//     btnEl.style.setProperty("--yPos", y + "px");
+// });
